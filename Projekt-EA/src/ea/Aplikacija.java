fd1632e2021af8e55e835c0942d5c1e4bf438a14
Marijan Šuflaj <msufflaj32@gmail.com>
2011-10-15 01:44:39 +0200

    Tesitram ovaj .gitignore

---
+++ Projekt-EA/src/ea/Aplikacija.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package ea;
+
+import javax.swing.JApplet;
+
+/**
+ * @author Marijan Šuflaj <msufflaj32@gmail.com>
+ *
+ */
+public class Aplikacija extends JApplet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4803571029460862673L;
+
+}
